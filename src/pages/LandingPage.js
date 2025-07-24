75dd8ae1593018867f60a99c12ba1be2009bcc93
RDAS-SOLUTIONS-LIMITED <sobandeoluwatonie@gmail.com>
2025-07-24 22:53:24 +0100

    Remove index.html file and update LandingPage link to point to KYC form; refresh ESLint cache and update binary cache files.

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -16,7 +16,7 @@ const LandingPage = () => {
             </p>
             <div className="flex flex-col sm:flex-row gap-4 justify-center">
               <Link
-                to="/dashboard"
+                to="/kyc-form"
                 className="bg-white text-blue-600 px-8 py-3 rounded-lg font-semibold hover:bg-gray-100 transition duration-300"
               >
                 Get Started
