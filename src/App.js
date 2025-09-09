4648d1112f00ce21d599da805150a2e3f623eec0
ofofonon <ofinaboy1@gmail.com>
2025-09-09 02:03:05 -0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { UserProvider } from './context/UserContext';
 import Navbar from './components/layout/Navbar';
 import Login from './pages/auth/Login';
@@ -15,48 +15,46 @@ import KYCForm from './components/forms/KYCForm';
 function App() {
   return (
     <UserProvider>
-      <Router>
-        <div className="min-h-screen bg-gray-50">
-          <Routes>
-            {/* Public routes */}
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/forgot-password" element={<ForgotPassword />} />
-            <Route path="/verify-otp" element={<VerifyOTP />} />
-            <Route path="/reset-password" element={<ResetPassword />} />
+      <div className="min-h-screen bg-gray-50">
+        <Routes>
+          {/* Public routes */}
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/verify-otp" element={<VerifyOTP />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
 
-            {/* Protected routes */}
-            <Route
-              path="/dashboard"
-              element={
-                <>
-                  <Navbar />
-                  <Dashboard />
-                </>
-              }
-            />
-            <Route
-              path="/profile"
-              element={
-                <>
-                  <Navbar />
-                  <Profile />
-                </>
-              }
-            />
-            <Route
-              path="/kyc"
-              element={
-                <>
-                  <Navbar />
-                  <KYCForm />
-                </>
-              }
-            />
-          </Routes>
-        </div>
-      </Router>
+          {/* Protected routes */}
+          <Route
+            path="/dashboard"
+            element={
+              <>
+                <Navbar />
+                <Dashboard />
+              </>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <>
+                <Navbar />
+                <Profile />
+              </>
+            }
+          />
+          <Route
+            path="/kyc"
+            element={
+              <>
+                <Navbar />
+                <KYCForm />
+              </>
+            }
+          />
+        </Routes>
+      </div>
     </UserProvider>
   );
 }
